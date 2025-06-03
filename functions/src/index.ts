1de53d383bbb1275d1d87b6334f3ec18e25f5bac
Sterling <sterling1@hotmail.ca>
2025-06-03 02:44:30 -0400

    Added daily schedule function to enable the new daily anime.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,10 +9,10 @@
 
 // import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
-import { auth } from "firebase-functions/v1";
-
 import { initializeApp } from "firebase-admin/app";
+import { auth } from "firebase-functions/v1";
 import { getFirestore } from "firebase-admin/firestore";
+import { onSchedule } from "firebase-functions/scheduler";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -24,10 +24,22 @@ import { getFirestore } from "firebase-admin/firestore";
 
 initializeApp();
 
-// const db = getFirestore();
+const db = getFirestore();
 
+const globalRef = db.collection("other").doc("global")
+//
 export interface StatsAnime {
     gamesPlayed: number,
+    //Updated when played
+    // if gameData.isDaily == true then +1
+    dailyPlayStreak: number,
+    //if isDaily and won then + 1
+    dailyWinStreak: number,
+    //Checked and updates daily when app opened
+    //If gameData.day - lastPlayed > 1 then resetDaily streak\
+    //Or if daily - lastPlayed > 1 then resetDaily
+    //Show lost streak alert
+    lastPlayed: number,
     firstFrameGuesses: number,
     secondFrameGuesses: number,
     thirdFrameGuesses: number,
@@ -41,12 +53,15 @@ export interface StatsAnime {
 
 
 //DOes this need to be async?
-exports.createFirestoreDocuments = auth.user().onCreate(async (user, res) => {
+exports.createFirestoreDocuments = auth.user().onCreate(async (user) => {
     // TODO: Add prepopualted list.
     // add 0s for the amount of games called but missing from list?
 
     let stats: StatsAnime = {
         gamesPlayed: 0,
+        dailyPlayStreak: 0,
+        dailyWinStreak: 0,
+        lastPlayed: -1,
         firstFrameGuesses: 0,
         secondFrameGuesses: 0,
         thirdFrameGuesses: 0,
@@ -54,15 +69,32 @@ exports.createFirestoreDocuments = auth.user().onCreate(async (user, res) => {
         fifthFrameGuesses: 0,
         sixthFrameGuesses: 0,
         failedGuesses: 0,
-        completionList: []
+        completionList: [],
     }
 
     // const docRef = await getFirestore()
-    const docRef = await getFirestore()
+    const docRef = db
         .collection("UserStatsAnime")
-        .doc(user.uid)
+        .doc(user.uid);
     
-    docRef.set(stats)
+    docRef.set(stats);
 
-    logger.log("Result", docRef.id)
+    logger.log("Result", docRef.id);
+});
+
+exports.updateDaily = onSchedule({schedule:"every day 01:36", timeZone: "America/Toronto"}, async (event) => {
+    const animeGamesRef = db.collection("AnimeFrameGuesser");
+    //Gets the next daily game which is disabled, and the smallest day.
+    const newGameQuery = animeGamesRef.where("enabled", "==", false).orderBy("day","asc").limit(1);
+    const newGameSnapshot = await newGameQuery.get();
+    
+    if(!newGameSnapshot.empty){
+        const gameDoc = newGameSnapshot.docs[0].ref
+        const gameDay = newGameSnapshot.docs[0].data()["day"]
+        gameDoc.update({enabled: true})
+        globalRef.update({day: gameDay})
+        logger.log("Daily game updated.")
+    }else{
+        logger.log("uh oh something went wrong.")
+    }
 });
